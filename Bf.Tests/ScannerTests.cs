88cd0fdd8d64fd713789cddea70bd16678da52c5
unsigned-wrong-wrong-int <59091082+unsigned-wrong-wrong-int@users.noreply.github.com>
2021-03-27 18:49:09 +0900

    Add tests for loop syntax

--- Bf.Tests/ScannerTests.cs
+++ Bf.Tests/ScannerTests.cs
@@ -13,6 +13,7 @@ namespace Bf.Tests
       {
          consoleErr = Console.Error;
          stderr = new();
+         stderr.NewLine = "\n";
          Console.SetError(stderr);
       }
 
@@ -28,6 +29,7 @@ namespace Bf.Tests
 
          Assert.False(scanner.MoveNext());
 
+         Assert.True(scanner.IsValid);
          var error = stderr.ToString();
          Assert.Empty(error);
       }
@@ -48,8 +50,152 @@ namespace Bf.Tests
 
          Assert.False(scanner.MoveNext());
 
+         Assert.True(scanner.IsValid);
          var error = stderr.ToString();
          Assert.Empty(error);
+      }
+
+      [Fact]
+      internal void AcceptsPairedBrackets()
+      {
+         var scanner = new Scanner(new[] { (byte)'[', (byte)']', });
+
+         Assert.True(scanner.MoveNext());
+         Assert.Equal(Token.BeginLoop, scanner.Current);
+
+         Assert.True(scanner.MoveNext());
+         Assert.Equal(Token.EndLoop, scanner.Current);
+
+         Assert.False(scanner.MoveNext());
+
+         Assert.True(scanner.IsValid);
+         var error = stderr.ToString();
+         Assert.Empty(error);
+      }
+
+      [Fact]
+      internal void ChecksForClosingBracket()
+      {
+         var scanner = new Scanner(new[] { (byte)'[' });
+
+         Assert.True(scanner.MoveNext());
+         Assert.Equal(Token.BeginLoop, scanner.Current);
+
+         Assert.False(scanner.MoveNext());
+
+         Assert.False(scanner.IsValid);
+         var error = stderr.ToString();
+         Assert.Equal("Syntax Error: Unmatched [ at 1:1\n", error);
+      }
+
+      [Fact]
+      internal void ChecksForOpeningBracket()
+      {
+         var scanner = new Scanner(new[] { (byte)']' });
+
+         Assert.True(scanner.MoveNext());
+         Assert.Equal(Token.InvalidBracket, scanner.Current);
+
+         Assert.False(scanner.MoveNext());
+
+         Assert.False(scanner.IsValid);
+         var error = stderr.ToString();
+         Assert.Equal("Syntax Error: Unmatched ] at 1:1\n", error);
+      }
+
+      static void AssertTokens(ReadOnlySpan<Token> expected, bool isValid,
+         string input)
+      {
+         var bytes = System.Text.Encoding.UTF8.GetBytes(input);
+         var scanner = new Scanner(bytes);
+
+         foreach (var token in expected)
+         {
+            Assert.True(scanner.MoveNext());
+            Assert.Equal(token, scanner.Current);
+         }
+
+         Assert.False(scanner.MoveNext());
+
+         Assert.Equal(isValid, scanner.IsValid);
+      }
+
+      [Fact]
+      internal void HandlesNestedBrackets()
+      {
+         AssertTokens(
+            new[] {
+               Token.BeginLoop,
+               Token.BeginLoop,
+               Token.EndLoop,
+               Token.EndLoop,
+            },
+            true,
+            "[[]]"
+         );
+
+         AssertTokens(
+            new[] {
+               Token.BeginLoop,
+               Token.BeginLoop,
+               Token.BeginLoop,
+               Token.BeginLoop,
+               Token.EndLoop,
+               Token.EndLoop,
+               Token.EndLoop,
+               Token.EndLoop,
+            },
+            true,
+            "[[[[]]]]"
+         );
+
+         AssertTokens(
+            new[] {
+               Token.BeginLoop,
+               Token.BeginLoop,
+               Token.EndLoop,
+               Token.BeginLoop,
+               Token.BeginLoop,
+               Token.EndLoop,
+               Token.EndLoop,
+               Token.EndLoop,
+            },
+            true,
+            "[[][[]]]"
+         );
+      }
+
+      [Fact]
+      internal void DetectsAllUnbalancedBrackets()
+      {
+         AssertTokens(
+            new[] {
+               Token.InvalidBracket,
+               Token.InvalidBracket,
+               Token.BeginLoop,
+               Token.EndLoop,
+               Token.InvalidBracket,
+               Token.BeginLoop,
+               Token.BeginLoop,
+               Token.BeginLoop,
+               Token.EndLoop,
+               Token.BeginLoop,
+            },
+            false,
+            "]][]][[[]["
+         );
+
+         var error = stderr.ToString();
+         Assert.Equal(
+            @"Syntax Error: Unmatched ] at 1:1
+Syntax Error: Unmatched ] at 1:2
+Syntax Error: Unmatched ] at 1:5
+Syntax Error: Unmatched [ at 1:10
+Syntax Error: Unmatched [ at 1:7
+Syntax Error: Unmatched [ at 1:6
+",
+            error
+         );
       }
    }
 }
