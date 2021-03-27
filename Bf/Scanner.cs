e81fa1c8ecd7114e237bb8677b840c2fc2633e87
unsigned-wrong-wrong-int <59091082+unsigned-wrong-wrong-int@users.noreply.github.com>
2021-03-27 13:48:51 +0900

    Add Scanner

---
+++ Bf/Scanner.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+
+namespace Bf
+{
+   enum Token : int
+   {
+      Increment = '+',
+      Decrement = '-',
+      MoveRight = '>',
+      MoveLeft = '<',
+      Write = '.',
+      Read = ',',
+      BeginLoop = '[',
+      EndLoop = ']',
+      InvalidBracket = -1,
+   }
+
+   ref struct Scanner
+   {
+      readonly ReadOnlySpan<byte>.Enumerator inner;
+
+      int line;
+      int column;
+      readonly Stack<(int line, int column)> loopStarts;
+
+      bool error;
+
+      public Scanner(ReadOnlySpan<byte> source)
+      {
+         inner = source.GetEnumerator();
+         line = column = 1;
+         loopStarts = new();
+         error = false;
+      }
+
+      public bool MoveNext()
+      {
+         if (!inner.MoveNext())
+         {
+            foreach (var (line, column) in loopStarts)
+            {
+               Error('[', line, column);
+            }
+            loopStarts.Clear();
+            return false;
+         }
+         return true;
+      }
+
+      public Token Current
+      {
+         get
+         {
+            var value = (Token)inner.Current;
+            switch (value)
+            {
+               case Token.BeginLoop:
+                  loopStarts.Push((line, column));
+                  goto default;
+               case Token.EndLoop:
+                  if (!loopStarts.TryPop(out _))
+                  {
+                     Error(']', line, column);
+                     value = Token.InvalidBracket;
+                  }
+                  goto default;
+               default:
+                  ++column;
+                  break;
+               case (Token)'\n':
+                  ++line;
+                  column = 1;
+                  break;
+            }
+            return value;
+         }
+      }
+
+      public bool IsValid => !error;
+
+      void Error(char c, int line, int column)
+      {
+         Console.Error.WriteLine(
+            $"Syntax Error: Unmatched {c} at {line}:{column}");
+         error = true;
+      }
+   }
+}
