ccd0bf63f5cf6f2c2af502464f461b869013a59d
unsigned-wrong-wrong-int <59091082+unsigned-wrong-wrong-int@users.noreply.github.com>
2021-04-18 17:30:49 +0900

    Use Analyzer in Interpreter.Parse()

--- Bf/Interpreter.cs
+++ Bf/Interpreter.cs
@@ -4,6 +4,13 @@ namespace Bf
 {
    class Interpreter
    {
+      readonly Analyzer.Analyzer analyzer;
+
+      public Interpreter()
+      {
+         analyzer = new();
+      }
+
       public bool Parse(ReadOnlySpan<byte> source)
       {
          var scanner = new Scanner(source);
@@ -12,20 +19,43 @@ namespace Bf
             switch (scanner.Current)
             {
                case Token.Increment:
+                  analyzer.Increment();
                   break;
                case Token.Decrement:
+                  analyzer.Decrement();
                   break;
                case Token.MoveRight:
+                  analyzer.MoveRight();
                   break;
                case Token.MoveLeft:
+                  analyzer.MoveLeft();
                   break;
                case Token.Write:
+                  analyzer.Write();
                   break;
                case Token.Read:
+                  analyzer.Read();
                   break;
                case Token.BeginLoop:
+                  if (!analyzer.BeginLoop())
+                  {
+                     goto skipCurrentLoop;
+                  }
                   break;
                case Token.EndLoop:
+                  if (!analyzer.EndLoop())
+                  {
+                     goto skipCurrentLoop;
+                  }
+                  break;
+               skipCurrentLoop:
+                  while (scanner.MoveNext())
+                  {
+                     if (scanner.Current == Token.EndLoop)
+                     {
+                        break;
+                     }
+                  }
                   break;
                case Token.InvalidBracket:
                   while (scanner.MoveNext()) { }
