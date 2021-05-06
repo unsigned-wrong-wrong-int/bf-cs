37e55abe971abb3904f00b943d3da6c4ae17de87
unsigned-wrong-wrong-int <59091082+unsigned-wrong-wrong-int@users.noreply.github.com>
2021-05-06 17:52:20 +0900

    Add Analyzer.Compiler

---
+++ Bf/Analyzer/Compiler.cs
@@ -0,0 +1,29 @@
+using System;
+using Bf.Core;
+
+namespace Bf.Analyzer
+{
+   class Compiler
+   {
+      readonly Pointer pointer;
+
+      public Compiler(Pointer pointer)
+      {
+         this.pointer = pointer;
+      }
+
+      public Action Compile<R>(R runtime) where R : IRuntime
+      {
+         var entryPoint = runtime.CreateEntryPoint();
+         Builder builder = new(entryPoint);
+         builder.Begin();
+         var current = pointer;
+         do {
+            pointer.Emit(builder);
+            current = current.Next;
+         } while (current is not null);
+         builder.End();
+         return entryPoint.Compile();
+      }
+   }
+}
